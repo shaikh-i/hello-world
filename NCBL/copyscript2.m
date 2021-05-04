12dbc4ca433c55c6bc4d9e9e22f917849a7d5744
shaikh-i <47722928+shaikh-i@users.noreply.github.com>
2021-05-04 13:46:05 -0600

    Create copyscript2.m
    MATLAB script to convert pressure matrix into Node-RED usable txt

---
+++ NCBL/copyscript2.m
@@ -0,0 +1,18 @@
+%% pre function commands
+close all;
+clc;
+clear;
+load("C:\Users\the_i\Desktop\NCBL\Data.mat");
+fid = fopen("test2.txt",'w+');
+%% function commands
+imgn = 40;
+for x = 1:imgn
+    for z = ((x*160)-15):x*160
+        for y = 1:16
+            fprintf(fid, '%d\n', FSA_pressure(z,y));
+        end
+    end
+end
+%% post function commands
+fclose(fid);
+fprintf("Success! Copy command complete. \n")
